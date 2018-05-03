dfe387634def2902fd8b6eeed25d217acce5d78c
정예은 <yeeun@jeong-yeeun-ui-MacBook-Pro.local>
2018-05-03 13:41:07 +0900

    예은 todo(오류포함)

---
+++ todo.js
@@ -0,0 +1,22 @@
+var title = document.getElementById('title');
+var list = document.getElementById('list');
+var li = list.getElementsByTagName('li');
+var addBtn = document.getElementById('add-btn');
+
+for(var i = 0; i < li.length; i++) {
+    li[i].addEventListener('click', activeItem);
+}
+
+function activeItem() {
+    title.innerHTML = this.innerHTML;
+    for(var i = 0; i < li.length; i++) {
+        li[i].classList.remove('active');
+    }
+    this.classList.add('active');
+}
+
+addBtn.addEventListener('click', function () {
+    var txt = prompt('할일을 입력하시오');
+    var temp = list.innerHTML;
+    list.innerHTML = temp + '<li>' + txt + '</li>';
+});
